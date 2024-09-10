b786787b2dbb5f6ba7709983e8929d19bc385d00
Ankita <ankitaprajapati8090@gmail.com>
2024-09-10 13:13:41 +0530

    Update App.js

--- dashboard/components/App.js
+++ dashboard/components/App.js
@@ -1,1 +1,26 @@
+App.js
+
+import React from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
+const Apps = () => {
+  return (
+    <div>
+      <form>
+        <TextField
+          style={{ width: "80%" }}
+          fullWidth
+          label="Search for apps"
+          id="fullWidth"
+        />
+        <Button style={{ marginLeft: "20px" }} variant="contained" size="large">
+          Search
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default Apps;
 
