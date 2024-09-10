4000e4da17c6651c4cc9a801d5cefdbd8bc39cf1
Ankita <ankitaprajapati8090@gmail.com>
2024-09-10 13:00:22 +0530

    Update PositionsModel.js

--- backend/models/PositionsModel.js
+++ backend/models/PositionsModel.js
@@ -1,1 +1,9 @@
 PositionsModel.js
+
+const {model} = require("mongoose");
+
+const { PositionsSchema } = require("../schemas/PositionsSchema"); 
+
+const PositionsModel = new model("position", PositionsSchema);
+
+module.exports = {PositionsModel};
