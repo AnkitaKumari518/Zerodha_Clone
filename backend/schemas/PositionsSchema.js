021b6909223e8450204a883e97e4c7c1866f1e31
Ankita <ankitaprajapati8090@gmail.com>
2024-09-10 13:02:33 +0530

    Update PositionsSchema.js

--- backend/schemas/PositionsSchema.js
+++ backend/schemas/PositionsSchema.js
@@ -1,1 +1,17 @@
+PositionsSchema.js
+
+const { Schema } = require("mongoose");
+
+const PositionsSchema = new Schema({
+  product: String,
+  name: String,
+  qty: Number,
+  avg: Number,
+  price: Number,
+  net: String,
+  day: String,
+  isLoss: Boolean,
+});
+
+module.exports = { PositionsSchema };
 
