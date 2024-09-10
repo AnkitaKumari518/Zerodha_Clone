c84344992626df0c9f6f86dc48d3113a3455e01d
Ankita <ankitaprajapati8090@gmail.com>
2024-09-10 13:01:09 +0530

    Update HoldingsSchema.js

--- backend/schemas/HoldingsSchema.js
+++ backend/schemas/HoldingsSchema.js
@@ -1,1 +1,15 @@
+HoldingsSchema.js
+
+const { Schema } = require("mongoose");
+
+const HoldingsSchema = new Schema({
+  name: String,
+  qty: Number,
+  avg: Number,
+  price: Number,
+  net: String,
+  day: String,
+});
+
+module.exports = {HoldingsSchema};
 
