501645e456973d93ce6eee2e64472b0d6f4c0dc5
Ankita <ankitaprajapati8090@gmail.com>
2024-09-10 13:36:28 +0530

    Add files via upload

---
+++ frontend/landing_page/support/CreateTicket.js
@@ -0,0 +1,412 @@
+import React from "react";
+
+function CreateTicket() {
+  return (
+    <div className="container">
+      <div className="row p-5 mt-5 mb-5">
+        <h1 className="fs-2" style={{ color: "#424242" }}>
+          To create a ticket, select a relevant topic
+        </h1>
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4 className="" style={{ color: "#424242" }}>
+            <i class="fa fa-plus-circle" aria-hidden="true"></i>
+            Account Opening
+          </h4>
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            Getting started
+          </a>
+          <br />
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            Online
+          </a>
+          <br />
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            Offline
+          </a>
+          <br />
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            Charges
+          </a>
+          <br />
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            Company, Partnership and HUF
+          </a>
+          <br />
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            Non Resident Indian (NRI)
+          </a>
+          <br />
+        </div>
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4 className="" style={{ color: "#424242" }}>
+          <i class="fa fa-user-o" aria-hidden="true"></i>
+            Your Zerodha Account
+          </h4>
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            Login credentials
+          </a>
+          <br />
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            Your Profile 
+          </a>
+          <br />
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            Account modification and segment addition
+          </a>
+          <br />
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            CMR & DP ID
+          </a>
+          <br />
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            Nomination
+          </a>
+          <br />
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            Transfer and conversion of shares
+          </a>
+          <br />
+        </div>
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4 className="" style={{ color: "#424242" }}>
+          <i class="fa fa-bar-chart" aria-hidden="true"></i>
+          Trading and Markets
+          </h4>
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            Trading FAQs
+          </a>
+          <br />
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            Kite
+          </a>
+          <br />
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            Margins
+          </a>
+          <br />
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            Product and order types
+          </a>
+          <br />
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            Corporate actions
+          </a>
+          <br />
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            Kite features
+          </a>
+          <br />
+        </div>
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4 className="" style={{ color: "#424242" }}>
+          <i class="fa fa-window-maximize" aria-hidden="true"></i>
+          Funds
+          </h4>
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            Fund withdrawal
+          </a>
+          <br />
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            Adding funds
+          </a>
+          <br />
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            Adding bank accounts
+          </a>
+          <br />
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            eMandates
+          </a>
+        </div>
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4 className="" style={{ color: "#424242" }}>
+          <i class="fa fa-circle-o-notch" aria-hidden="true"></i>
+          Console
+          </h4>
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            IPO
+          </a>
+          <br />
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            Portfolio
+          </a>
+          <br />
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            Funds statement
+          </a>
+          <br />
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+           Profile
+          </a>
+          <br />
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            Reports
+          </a>
+          <br />
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            Referral program
+          </a>
+          <br />
+        </div>
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4 className="" style={{ color: "#424242" }}>
+          <i class="fa fa-circle-thin" aria-hidden="true"></i>
+          Coin
+          </h4>
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            Understanding mutual funds and Coin
+          </a>
+          <br />
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            Coin app
+          </a>
+          <br />
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            Coin web
+          </a>
+          <br />
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            Transactions and reports
+          </a>
+          <br />
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              lineHeight: "2.5rem",
+              fontSize: "20px",
+            }}
+          >
+            National Pension Scheme (NPS)
+            </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CreateTicket;
