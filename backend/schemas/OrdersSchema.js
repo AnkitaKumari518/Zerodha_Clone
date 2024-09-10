737f4713c62f115d96b0701d5e0d2ce5b622fff0
Ankita <ankitaprajapati8090@gmail.com>
2024-09-10 13:01:40 +0530

    Update OrdersSchema.js

--- backend/schemas/OrdersSchema.js
+++ backend/schemas/OrdersSchema.js
@@ -1,1 +1,13 @@
+OrdersSchema.js
+
+const { Schema } = require("mongoose");
+
+const OrdersSchema = new Schema({
+  name: String,
+  qty: Number,
+  price: Number,
+  mode: String,
+});
+
+module.exports = { OrdersSchema };
 
